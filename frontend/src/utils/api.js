04d63c7988573548ea75b20f153bbe93683ec896
yashshukla2109 <yashshukla609@gmail.com>
2026-09-09 01:38:02 +0530

    Fix API URL to use env var

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:3001/api/v1';
+const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:3001/api/v1';
 
 export const api = axios.create({
   baseURL: API_BASE,
